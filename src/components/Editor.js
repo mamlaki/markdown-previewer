c9afdc8d217b5bce75d339a308ddd1b42a1ec431
mamlaki <melekredwan@icloud.com>
2023-10-17 04:24:27 -0600

    Editor.js: Passed through markdown state as props.

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,10 @@
-export default function Editor() {
+export default function Editor({ markdown, setMarkdown }) {
   return (
     <div className='editor-container'>
       <textarea 
         id='editor'
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
       />
     </div>
   )
