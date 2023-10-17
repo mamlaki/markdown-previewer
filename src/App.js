920bbd6d9078245deda7b5354e9aa2745439898a
mamlaki <melekredwan@icloud.com>
2023-10-17 04:09:36 -0600

    App.js/Editor.js: Moved editor code to separate component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import Editor from './components/Editor'
 import './App.css'
 
 export default function App() {
   return (
     <div className='App'>
-      <div className='editor-container'>
-        <textarea 
-          id='editor'
-        />
-      </div>
+      <Editor />
       <div className='preview-container' id='preview'>
 
       </div>
