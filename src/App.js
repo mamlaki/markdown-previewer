607bc36cfd845b8c81a9b1fe86cea903ab4c5f5b
mamlaki <melekredwan@icloud.com>
2023-10-17 04:11:04 -0600

    App.js/Preview.js: Moved preview code into separate component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Editor from './components/Editor'
+import Preview from './components/Preview'
 import './App.css'
 
 export default function App() {
   return (
     <div className='App'>
       <Editor />
-      <div className='preview-container' id='preview'>
-
-      </div>
+      <Preview />
     </div>
   )
 }
---
+++ src/components/Preview.js
@@ -0,0 +1,7 @@
+export default function Preview() {
+  return (
+    <div id='preview'>
+      
+    </div>
+  )
+}
