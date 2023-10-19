f7c2fe0162629d10209368bda2320e83f1830c0d
mamlaki <melekredwan@icloud.com>
2023-10-18 22:25:15 -0600

    Editor.js/Preview.js: Added fCC icons to headers.

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,12 @@
+import { FaFreeCodeCamp } from 'react-icons/fa6'
+
 export default function Editor({ markdown, setMarkdown }) {
   return (
     <div className='editor-container'>
-      <div className='editor-header'>Editor</div>
+      <div className='editor-header'>
+        <FaFreeCodeCamp />
+        Editor
+      </div>
       <textarea 
         id='editor'
         value={markdown}
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,9 +1,13 @@
 import { marked } from 'marked'
+import { FaFreeCodeCamp } from 'react-icons/fa6'
 
 export default function Preview({ markdown }) {
   return (
     <div className='preview-container'>
-      <div className='preview-header'>Previewer</div>
+      <div className='preview-header'>
+        <FaFreeCodeCamp />
+        Previewer
+      </div>
       <div id='preview' dangerouslySetInnerHTML={{ __html: marked(markdown)}}></div>
     </div>
   )
