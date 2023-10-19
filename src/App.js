31b848f0c4e9dfa4c331bf38e115cb97fca2d40f
mamlaki <melekredwan@icloud.com>
2023-10-19 02:58:32 -0600

    App.js: Fixed default editor text positioning.

--- src/App.js
+++ src/App.js
@@ -7,46 +7,46 @@ import './App.css'
 export default function App() {
   const [markdown, setMarkdown] = useState(`# Welcome to my React Markdown Previewer!
   
-  ## This is a sub-heading...
-  ### And here's some other cool stuff:
+## This is a sub-heading...
+### And here's some other cool stuff:
 
-  Heres some code, \`<div></div>\`, between 2 backticks.
+Heres some code, \`<div></div>\`, between 2 backticks.
 
-  \`\`\`
-  // this is multi-line code:
+\`\`\`
+// this is multi-line code:
 
-  function anotherExample(firstLine, lastLine) {
-    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-      return multiLineCode;
-    }
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
   }
-  \`\`\`
+}
+\`\`\`
 
-  You can also amke text **bold**... whoa!
-  Or _italic_.
-  Or... wait for it... **_both!_**
-  And feel free to go crazy ~~crossing stuff out~~.
+You can also amke text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
 
-  There's also [links](https://www.freecodecamp.org), and
-  > Block Quotes!
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
 
-  And if you want to get really crazy, even tables:
+And if you want to get really crazy, even tables:
 
-  Wild Header | Crazy Header | Another Header?
-  ------------ | ------------- | -------------
-  Your content can | be here, and it | can be here....
-  And here. | Okay. | I think we get it.
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
 
-  - And of course there are lists.
-    - Some are bulleted.
-      - With different indentation levels.
-        - That look like this.
+- And of course there are lists.
+  - Some are bulleted.
+    - With different indentation levels.
+      - That look like this.
 
-  1. And there are numbered lists too.
-  1. Use just 1s if you want!
-  1. And last but not least, let's not forget embedded images:
-  
-  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
   `)
   const [expandedComponent, setExpandedComponent] = useState('')
   const [customHeight, setCustomHeight] = useState(null)
